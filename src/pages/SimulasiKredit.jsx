764ed2e93f68bb9c768bb5eda36a3d89da7e43f8
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-03-20 15:21:34 +0700

    feat: implement credit simulation form with validation and result display

--- src/pages/SimulasiKredit.jsx
+++ src/pages/SimulasiKredit.jsx
@@ -1,25 +1,92 @@
 import { useState } from "react";
+import Alert from "../components/Alert";
+import InputField from "../components/InputField";
+import SelectField from "../components/SelectField";
 
 export default function SimulasiKredit() {
-  const [gaji, setGaji] = useState("");
-  const pajak = 0.11;
-  const totalGaji = gaji - gaji * pajak;
+  const [tenor, setTenor] = useState("");
+  const [harga, setHarga] = useState("");
+  const [nama, setNama] = useState("");
+  const [errors, setErrors] = useState({});
+  const [hasilSimulasi, setHasilSimulasi] = useState(null);
+
+  const validate = () => {
+    let newErrors = {};
+    if (!nama) {
+      newErrors.nama = "Nama wajib diisi.";
+    } else if (!/^[A-Za-z\s]+$/.test(nama)) {
+      newErrors.nama =
+        "Nama tidak boleh mengandung angka atau karakter khusus.";
+    }
+
+    if (!harga || parseFloat(harga) <= 0) {
+      newErrors.harga = "Harga kendaraan wajib diisi.";
+    } else if (parseFloat(harga) <= 0) {
+      newErrors.harga = "Harga kendaraan harus lebih dari 0.";
+    }
+
+    if (!tenor) newErrors.tenor = "Silakan pilih tenor.";
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const hitungKredit = () => {
+    if (!validate()) return;
+    const bunga = 0.05;
+    const tenorBulan = parseInt(tenor);
+    const hargaKendaraan = parseFloat(harga);
+    const totalHarga = hargaKendaraan * (1 + bunga * (tenorBulan / 12));
+    const cicilanPerBulan = totalHarga / tenorBulan;
+    setHasilSimulasi({ totalHarga, cicilanPerBulan });
+  };
+
   return (
-    <div className="m-5 flex min-h-screen flex-col items-center justify-center bg-gray-100 p-5">
-      <div className="w-96 rounded-lg bg-white p-6 shadow-lg">
-        <h2 className="mb-4 text-center text-2xl font-semibold text-gray-700">
-          Hitung Gaji Bersih
+    <div className="m-5 flex min-h-screen flex-col items-center justify-center p-5">
+      <div className="w-150 rounded-lg bg-white p-6 shadow-xl">
+        <h2 className="mb-4 text-2xl font-bold text-blue-800">
+          Simulasi Kredit Mobil
         </h2>
+        <InputField
+          label="Nama Pemohon"
+          type="text"
+          placeholder="Masukkan nama"
+          onChange={(e) => setNama(e.target.value)}
+        />
+        {errors.nama && <Alert tipe="error" pesan={errors.nama} />}
+        <InputField
+          label="Harga Kendaraan"
+          type="number"
+          placeholder="Masukkan harga"
+          onChange={(e) => setHarga(e.target.value)}
+        />
+        {errors.harga && <Alert tipe="error" pesan={errors.harga} />}
+        <SelectField
+          label="Tenor (Bulan)"
+          onChange={(e) => setTenor(e.target.value)}
+        />
+        {errors.tenor && <Alert tipe="error" pesan={errors.tenor} />}
 
-        <div className="mb-4">{/* Input Gaji */}</div>
 
-        <div className="mb-4">
-          <label className="mb-1 block font-medium text-gray-700">
-            Pajak: <b class="text-red-500">11%</b>
-          </label>
-        </div>
+        <button
+          onClick={hitungKredit}
+          className="mt-4 w-full rounded bg-blue-500 py-2 text-white"
+        >
+          Tampilkan Hasil
+        </button>
 
-        {/*Komponen Kondisi */}
+        
+        {hasilSimulasi && (
+          <div className="mt-4 rounded bg-slate-100 p-4 shadow-md">
+            <h3 className="text-lg font-semibold text-blue-900">
+              Hasil Simulasi Kredit
+            </h3>
+            <p>Total Harga: Rp {hasilSimulasi.totalHarga.toLocaleString()}</p>
+            <p>
+              Cicilan Per Bulan: Rp{" "}
+              {hasilSimulasi.cicilanPerBulan.toLocaleString()}
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
