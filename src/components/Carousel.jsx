a095964a0313d551a1d78c8dd41b21f16ac81280
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-03-28 22:00:18 +0700

    feat: update Carousel component with new images and remove unused files

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,10 +2,9 @@ import "../css/styles.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
-import "swiper/css/pagination";
-import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import { Autoplay, Navigation } from "swiper/modules";
 import { useRef, useEffect } from "react";
-import { ChevronLeft, ChevronRight } from "lucide-react"; // Tambahkan ikon
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const Carousel = () => {
   const prevRef = useRef(null);
@@ -22,45 +21,38 @@ const Carousel = () => {
       }
     };
 
-    setTimeout(updateSwiperNavigation, 500); // Delay agar swiper terinisialisasi
+    setTimeout(updateSwiperNavigation, 500);
   }, []);
 
   return (
-    <section className="main_visual relative h-auto overflow-hidden  mb-40">
+    <section className="main_visual relative h-auto overflow-hidden mb-40">
       <div className="swiper-container1 visSwiper relative">
         <Swiper
-          modules={[Autoplay, Navigation, Pagination]}
+          modules={[Autoplay, Navigation]}
           autoplay={{ delay: 3000, disableOnInteraction: false }}
           loop={true}
-          navigation={{
-            prevEl: prevRef.current,
-            nextEl: nextRef.current,
-          }}
-          pagination={{ type: "fraction" }}
-          className="w-full object-cover  h-[300px] max-h-[690px] sm:min-h-[400px] md:min-h-[600px] lg:min-h-[500px]"
+          navigation={{ prevEl: prevRef.current, nextEl: nextRef.current }}
+          className="w-full object-cover h-[400px] max-h-[690px] sm:min-h-[400px] md:min-h-[600px] lg:min-h-[500px]"
         >
-          <SwiperSlide className="flex items-center justify-center bg-red-500 text-2xl text-white">
-            Slide 1
+          <SwiperSlide className="flex items-center justify-center text-white">
+            <img src="/image/2.png" alt="gambar"/>
           </SwiperSlide>
-          <SwiperSlide className="flex items-center justify-center bg-blue-500 text-2xl text-white">
-            Slide 2
-          </SwiperSlide>
-          <SwiperSlide className="flex items-center justify-center bg-green-500 text-2xl text-white">
-            Slide 3
+          <SwiperSlide className="flex items-center justify-center text-white">
+            <img src="/image/1.png" alt="gambar" />
           </SwiperSlide>
         </Swiper>
 
         {/* Custom Navigation Buttons */}
         <button
           ref={prevRef}
-          className="absolute top-1/2 left-5 transform -translate-y-1/2 bg-white bg-opacity-50 text-black rounded-full p-3 z-10"
+          className="absolute top-1/2 left-5 transform -translate-y-1/2 bg-white bg-opacity-50 text-black rounded-full p-3 z-10 hidden lg:block"
         >
           <ChevronLeft size={24} />
         </button>
 
         <button
           ref={nextRef}
-          className="absolute top-1/2 right-5 transform -translate-y-1/2 bg-white bg-opacity-50 text-black rounded-full p-3 z-10"
+          className="absolute top-1/2 right-5 transform -translate-y-1/2 bg-white bg-opacity-50 text-black rounded-full p-3 z-10 hidden lg:block"
         >
           <ChevronRight size={24} />
         </button>
