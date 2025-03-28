96a247ecc7550ccdda361e1abf51a2b5a2e11f36
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-03-28 15:44:20 +0700

    feat: add ScrollToTop component to AppRoutes for improved navigation experience

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,16 +4,20 @@ import Cars from "../pages/Cars";
 import SimulasiKredit from "../pages/SimulasiKredit";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import ScrollToTop from "../components/ScrollToTop"; 
+
 
 const AppRoutes = () => {
   return (
     <Router>
       <Navbar />
+      <ScrollToTop /> {/* Tambahkan di sini */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cars" element={<Cars />} />
         <Route path="/simulasi-kredit" element={<SimulasiKredit />} />
       </Routes>
+      
       <Footer />
     </Router>
   );
