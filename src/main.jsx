dcdf3364cea91d8ab5624fa5ace42e3eae4c7e03
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-03-28 15:43:12 +0700

    feat: integrate Swiper for enhanced carousel functionality in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import "./css/styles.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 );
