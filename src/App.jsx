7033c0459714a038c2eb10ca7684afa7f842aff5
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-03-28 15:43:00 +0700

    refactor: clean up App component by removing unused state and fixing JSX syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
 import AppRoutes from "./routes/AppRoutes";
 import "./css/styles.css";
 
+
 function App() {
-  const [count, setCount] = useState(0);
+ 
 
   return (
     <>
-      <AppRoutes />;
+      <AppRoutes />
     </>
   );
 }
