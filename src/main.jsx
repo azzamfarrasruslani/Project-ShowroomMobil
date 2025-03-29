805356fc39f478fb17c6eb5c0d021d7ba3fcfaec
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-03-29 13:18:25 +0700

    refactor: remove unused Swiper imports from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import "./css/styles.css";
 import App from "./App.jsx";
 
