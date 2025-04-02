b833272fe3f2a201c447dfeb13157f64508ce381
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-04-02 14:29:13 +0700

    feat: wrap AppRoutes component in a div for improved layout structure

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,22 +4,23 @@ import Cars from "../pages/Cars";
 import SimulasiKredit from "../pages/SimulasiKredit";
 import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
-import ScrollToTop from "../components/common/ScrollToTop"; 
-
+import ScrollToTop from "../components/layout/ScrollToTop";
 
 const AppRoutes = () => {
   return (
-    <Router>
-      <Navbar />
-      <ScrollToTop /> 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cars" element={<Cars />} />
-        <Route path="/simulasi-kredit" element={<SimulasiKredit />} />
-      </Routes>
-      
-      <Footer />
-    </Router>
+    <div className="bg-gray-100">
+      <Router>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cars" element={<Cars />} />
+          <Route path="/simulasi-kredit" element={<SimulasiKredit />} />
+        </Routes>
+
+        <Footer />
+      </Router>
+    </div>
   );
 };
 
