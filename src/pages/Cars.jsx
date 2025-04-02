f15f9a0e3100d088e996d6366b89b4eef6eb04d6
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-04-02 14:28:53 +0700

    feat: update Cars component layout and improve car image sources

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -1,28 +1,62 @@
-import CarCard from "../components/CarCard";
+import CarCard from "../components/pages/cars/CarCard";
 
 const cars = [
   {
     id: 1,
     name: "Toyota Avanza",
     price: "Rp 200 juta",
-    image: "/images/avanza.jpg",
+    image:
+      "https://img.kcar.com/carpicture/carpicture05/pic6115/kcarM_61155288_001.jpg",
   },
   {
     id: 2,
     name: "Honda Civic",
     price: "Rp 400 juta",
-    image: "/images/civic.jpg",
+    image:
+      "https://img.kcar.com/carpicture/carpicture05/pic6115/kcarM_61152137_001.jpg",
+  },
+  {
+    id: 2,
+    name: "Honda Civic",
+    price: "Rp 400 juta",
+    image:
+      "https://img.kcar.com/carpicture/carpicture05/pic6115/kcarM_61152137_001.jpg",
+  },
+  {
+    id: 2,
+    name: "Honda Civic",
+    price: "Rp 400 juta",
+    image:
+      "https://img.kcar.com/carpicture/carpicture05/pic6115/kcarM_61152137_001.jpg",
+  },
+  {
+    id: 2,
+    name: "Honda Civic",
+    price: "Rp 400 juta",
+    image:
+      "https://img.kcar.com/carpicture/carpicture05/pic6115/kcarM_61152137_001.jpg",
+  },
+  {
+    id: 2,
+    name: "Honda Civic",
+    price: "Rp 400 juta",
+    image:
+      "https://img.kcar.com/carpicture/carpicture05/pic6115/kcarM_61152137_001.jpg",
   },
 ];
 
 const Cars = () => {
   return (
-    <div className="container mx-auto min-h-screen p-10">
-      <h1 className="mb-6 text-3xl font-bold">Daftar Mobil</h1>
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-        {cars.map((car) => (
-          <CarCard key={car.id} car={car} />
-        ))}
+    <div>
+      <div className="h-[400px] max-h-[690px] w-full object-cover sm:min-h-[400px] md:min-h-[600px] lg:min-h-[500px]">
+        <img src="/image/Sell_Car_Banner.png" alt="gambar" />
+      </div>
+      <div className="container mx-auto mt-10 mb-30 flex flex-col gap-6 px-4">
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-3">
+          {cars.map((car) => (
+            <CarCard key={car.id} car={car} />
+          ))}
+        </div>
       </div>
     </div>
   );
