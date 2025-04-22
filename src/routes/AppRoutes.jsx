5ee7eb1c4543d28ec52bc5374227b8a8d47ab0c1
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-04-22 19:42:07 +0700

    feat: update routing in AppRoutes to include BuyCars, SellCar, Faq, and ContactUs components

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
-import Cars from "../pages/Cars";
+import BuyCars from "../pages/BuyCars";
+import SellCar from "../pages/SellCars";
+import Faq from "../pages/Faq";
+import ContactUs from "../pages/ContactUs";
 import SimulasiKredit from "../pages/SimulasiKredit";
-import Navbar from "../components/layout/Navbar";
-import Footer from "../components/layout/Footer";
-import ScrollToTop from "../components/layout/ScrollToTop";
-
+import Navbar from "../layout/Navbar";
+import Footer from "../layout/Footer";
+import ScrollToTop from "../layout/ScrollToTop";
+import ErrorPage from "../pages/error/errorPage"; 
 const AppRoutes = () => {
   return (
     <div className="bg-gray-100">
@@ -14,8 +17,14 @@ const AppRoutes = () => {
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/cars" element={<Cars />} />
+          <Route path="/buy-cars" element={<BuyCars />} />
           <Route path="/simulasi-kredit" element={<SimulasiKredit />} />
+          <Route path="/sell-cars" element={<SellCar />} />
+          <Route path="/faq" element={<Faq />} />
+          <Route path="/kontak-kami" element={<ContactUs />} />
+          {/* Add more routes as needed */}
+          {/* 404 Not Found */}
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
 
         <Footer />
