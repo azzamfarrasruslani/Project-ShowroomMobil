78ed559e812a6bba1041439d3a0e9ad2b3e4d724
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-05-17 11:57:52 +0700

    feat: implement lazy loading for guest and admin routes with Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./css/styles.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Guest Pages
-import Home from "./pages/guest/Home";
-import BuyCars from "./pages/guest/BuyCars";
-import SellCar from "./pages/guest/SellCars";
-import Faq from "./pages/guest/Faq";
-import ContactUs from "./pages/guest/ContactUs";
-import SimulasiKredit from "./pages/guest/SimulasiKredit";
+// import Home from "./pages/guest/Home";
+// import BuyCars from "./pages/guest/BuyCars";
+// import SellCar from "./pages/guest/SellCars";
+// import Faq from "./pages/guest/Faq";
+// import ContactUs from "./pages/guest/ContactUs";
+// import SimulasiKredit from "./pages/guest/SimulasiKredit";
+
+const Home = React.lazy(() => import("./pages/guest/Home"));
+const BuyCars = React.lazy(() => import("./pages/guest/BuyCars"));
+const SellCars = React.lazy(() => import("./pages/guest/SellCars"));
+const Faq = React.lazy(() => import("./pages/guest/Faq"));
+const ContactUs = React.lazy(() => import("./pages/guest/ContactUs"));
+const SimulasiKredit = React.lazy(() => import("./pages/guest/SimulasiKredit"));
 
 // Admin Pages
 import Dashboard from "./pages/admin/Dashboard";
@@ -18,42 +25,43 @@ import Pembeli from "./pages/admin/Pembeli";
 import Orders from "./pages/admin/Orders";
 import TestDrive from "./pages/admin/TestDrive";
 
-
 // Auth Pages
-
 
 // Layouts
 import ScrollToTop from "./layout/ScrollToTop";
 import ErrorPage from "./pages/error/errorPage";
 import GuestLayout from "./layout/GuestLayout";
 import AdminLayout from "./layout/AdminLayout";
+import Loading from "./components/common/Loading";
 
 function App() {
   return (
     <div className="bg-gray-100">
       <ScrollToTop />
-      <Routes>
-        <Route element={<GuestLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/buy-cars" element={<BuyCars />} />
-          <Route path="/simulasi-kredit" element={<SimulasiKredit />} />
-          <Route path="/sell-cars" element={<SellCar />} />
-          <Route path="/faq" element={<Faq />} />
-          <Route path="/kontak-kami" element={<ContactUs />} />
-          <Route path="/error/:errorCode" element={<ErrorPage />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Route>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route element={<GuestLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/buy-cars" element={<BuyCars />} />
+            <Route path="/simulasi-kredit" element={<SimulasiKredit />} />
+            <Route path="/sell-cars" element={<SellCars />} />
+            <Route path="/faq" element={<Faq />} />
+            <Route path="/kontak-kami" element={<ContactUs />} />
+            <Route path="/error/:errorCode" element={<ErrorPage />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
 
-        <Route element={<AdminLayout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/customers" element={<Customers />} />
-          <Route path="/mobil" element={<Mobil />} />
-          <Route path="/pembeli" element={<Pembeli />} />
-          <Route path="/Orders" element={<Orders />} />
-          <Route path="/test-drive" element={<TestDrive />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Route>
-      </Routes>
+          <Route element={<AdminLayout />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/customers" element={<Customers />} />
+            <Route path="/mobil" element={<Mobil />} />
+            <Route path="/pembeli" element={<Pembeli />} />
+            <Route path="/Orders" element={<Orders />} />
+            <Route path="/test-drive" element={<TestDrive />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
+        </Routes>{" "}
+      </Suspense>
     </div>
   );
 }
