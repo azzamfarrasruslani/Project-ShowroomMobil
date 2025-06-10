093a87e6c21aa5fd9c83fb561a0509c7047f5e6c
azzamfarrasruslani <azzamfarrasrusl@gmail.com>
2025-06-10 10:28:39 +0700

    refactor: reorganize Home component structure for improved layout and responsiveness

--- src/pages/guest/Home.jsx
+++ src/pages/guest/Home.jsx
@@ -1,6 +1,6 @@
+
 import { useState } from "react";
 /* eslint-disable no-unused-vars */
-import { motion } from "framer-motion";
 import Carousel from "../../components/guest/home/Carousel";
 import SearchCar from "../../components/guest/home/SearchCar";
 import Ulasan from "../../components/guest/home/Ulasan";
@@ -10,14 +10,33 @@ const Home = () => {
   const [selectedType, setSelectedType] = useState(null);
   return (
     <>
-      {/* Hero Section */}
-      <section className="min-h-screen">
-        <Carousel />
-        <SearchCar />
+      <section className="relative">
+        {/* Carousel sebagai latar */}
+        <div className="relative h-[400px] overflow-hidden sm:h-[500px] md:h-[600px] lg:h-[700px] xl:h-[800px]">
+          <Carousel />
+        </div>
 
-        <Ulasan/>
-        <Faq />
+        {/* SearchCar mengambang di bawah carousel */}
+        {/* SearchCar untuk tampilan Desktop (absolute) */}
+        <div className="absolute inset-x-0 bottom-[-300px] z-10 hidden justify-center px-4 lg:flex xl:bottom-[-360px]">
+          <div className="w-full max-w-6xl sm:p-6">
+            <SearchCar />
+          </div>
+        </div>
+
+        {/* SearchCar untuk tampilan Mobile (di bawah Carousel) */}
+        <div className="mt-4 block px-4 lg:hidden">
+          <div className="mx-auto w-full max-w-6xl rounded-xl bg-white p-4 shadow-lg sm:p-6">
+            <SearchCar />
+          </div>
+        </div>
       </section>
+
+      {/* Spacer agar komponen berikut tidak tertutup */}
+      <div className="mt-[300px] sm:mt-[500px] md:mt-[550px] lg:mt-[580px] xl:mt-[400px]">
+        <Ulasan />
+        <Faq />
+      </div>
     </>
   );
 };
