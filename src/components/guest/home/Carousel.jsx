d68bdd9ac00a11a497fbc46758d8928e2cc480a1
azzamfarrasruslani <azzamfarrasrusl@gmail.com>
2025-06-10 10:28:19 +0700

    refactor: simplify Carousel component structure and improve styling

--- src/components/guest/home/Carousel.jsx
+++ src/components/guest/home/Carousel.jsx
@@ -1,3 +1,4 @@
+
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -24,37 +25,37 @@ const Carousel = () => {
   }, []);
 
   return (
-    <section className="main_visual relative h-auto overflow-hidden">
-      <div className="swiper-container1 visSwiper relative">
+    <section className="relative h-full overflow-hidden">
+      <div className="relative">
         <Swiper
           modules={[Autoplay, Navigation]}
           autoplay={{ delay: 3000, disableOnInteraction: false }}
           loop={true}
           navigation={{ prevEl: prevRef.current, nextEl: nextRef.current }}
-          className="h-[400px] max-h-[690px] w-full object-cover sm:min-h-[400px] md:min-h-[600px] lg:min-h-[500px]"
+          className="h-full w-full"
         >
-          <SwiperSlide className="flex items-center justify-center text-white">
-            <img src="/image/2.png" alt="gambar" />
-          </SwiperSlide>
-          <SwiperSlide className="flex items-center justify-center text-white">
-            <img src="/image/3.png" alt="gambar" />
-          </SwiperSlide>
-          <SwiperSlide className="flex items-center justify-center text-white">
-            <img src="/image/1.png" alt="gambar" />
-          </SwiperSlide>
+          {["2", "3", "1"].map((img, idx) => (
+            <SwiperSlide key={idx} className="flex items-center justify-center">
+              <img
+                src={`/image/${img}.png`}
+                alt={`gambar-${idx}`}
+                className="h-full w-full object-cover"
+              />
+            </SwiperSlide>
+          ))}
         </Swiper>
 
         {/* Custom Navigation Buttons */}
         <button
           ref={prevRef}
-          className="bg-opacity-50 absolute top-1/2 left-5 z-10 hidden -translate-y-1/2 transform rounded-full bg-white p-3 text-black lg:block"
+          className="bg-opacity-50 absolute top-1/2 left-3 z-10 hidden -translate-y-1/2 transform rounded-full bg-white p-3 text-black lg:block"
         >
           <ChevronLeft size={24} />
         </button>
 
         <button
           ref={nextRef}
-          className="bg-opacity-50 absolute top-1/2 right-5 z-10 hidden -translate-y-1/2 transform rounded-full bg-white p-3 text-black lg:block"
+          className="bg-opacity-50 absolute top-1/2 right-3 z-10 hidden -translate-y-1/2 transform rounded-full bg-white p-3 text-black lg:block"
         >
           <ChevronRight size={24} />
         </button>
